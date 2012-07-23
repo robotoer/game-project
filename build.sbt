f6c61f01ac2e86235dc9f75c55e5be33206cb573
Robert Chu <robert@wibidata.com>
2012-07-23 04:06:24 -0700

    Got Reactive working in MainActivity.

---
+++ build.sbt
@@ -0,0 +1,3 @@
+resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+
+libraryDependencies += "cc.co.scala-reactive" %% "reactive-core" % "0.2-SNAPSHOT"
--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -1,13 +1,36 @@
 package com.gameproject
 
-import _root_.android.app.Activity
-import _root_.android.os.Bundle
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
-class MainActivity extends Activity with TypedActivity {
+import reactive.EventSource
+import reactive.Observing
+
+/**
+ * Main activity demonstrating the usage of Android, Scala, and Reactive in one project.
+ *
+ * Note: The Observing trait is required in all classes that use the [[EventSource#foreach]] method.
+ */
+class MainActivity extends Activity with TypedActivity with Observing {
+  // Setup an EventSource (in an actual project a real subtype of EventSource should be used instead).
+  val es: EventSource[String] = new EventSource[String] { }
+
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
 
     findView(TR.textview).setText("hello, world!")
+    es.foreach(msg => Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show())
+  }
+
+  /**
+   * Observer for button. This fires an event for the test event source.
+   *
+   * @param view The view that caused this event.
+   */
+  def buttonObserver(view: View) {
+    es fire "foo msg!"
   }
 }
